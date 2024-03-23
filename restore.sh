ce95b5dbf0f13395cb362a48523bf7624c1df04e
Andrey Kuvshinov <38362418+asyslinux@users.noreply.github.com>
2024-03-23 18:22:49 +0700

    Update restore.sh

--- restore.sh
+++ restore.sh
@@ -3,9 +3,14 @@
 for FPN in `find $1/ -type f -not -wholename $1/recup_dir.*/report.xml`
 do
 
-SUM=`md5sum -z $FPN | cut -d " " -f1 | head -c -1`
-EXT=`echo -n $FPN | awk -F'[.]' '{print $NF}'`
-FNM=`echo -n $SUM.$EXT`
+EXT=`echo -n "$FPN" | awk -F'[.]' '{print $NF}'`
+
+if [ "$FPN" = "$EXT" ]; then
+FNM=`echo -n "$FPN" | rev | cut -d '/' -f1 | rev`
+else
+SUM=`md5sum -z "$FPN" | cut -d " " -f1 | head -c -1`
+FNM=`echo -n "$SUM.$EXT"`
+fi
 
 FL=${SUM:0:2}
 SL=${SUM:3:5}
