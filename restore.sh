bb80df00978ebcb3170e862c250969ca10fe9deb
Andrey Kuvshinov <38362418+asyslinux@users.noreply.github.com>
2024-03-23 17:44:33 +0700

    Update restore.sh

--- restore.sh
+++ restore.sh
@@ -1,17 +1,22 @@
 #!/bin/bash
 
-for X in `ls -1 $1/`
+for FPN in `find $1/ -type f -not -wholename $1/recup_dir.*/report.xml`
 do
 
-SUM=`md5sum -z $1/$X | cut -d " " -f1 | head -c -1`
-FNM=`echo -n $SUM | awk '{print $1".jpg"}'`
+SUM=`md5sum -z $FPN | cut -d " " -f1 | head -c -1`
+EXT=`echo -n $FPN | awk -F'[.]' '{print $NF}'`
+FNM=`echo -n $SUM.$EXT`
 
 FL=${SUM:0:2}
 SL=${SUM:3:5}
 
-mkdir -p $2/$FL/$SL
-mv $1/$X $2/$FL/$SL/$FNM
+mkdir -p "$2/$FL/$SL"
+mv "$FPN" "$2/$FL/$SL/$FNM"
 
 done
+
+if [ ! -z "$1" ]; then
+rm -rf $1/recup_dir.*
+fi
 
 #END
